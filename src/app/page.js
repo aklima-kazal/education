a520743ca235e7d666ff29a8b8b1c9934f7d2903
aklima-kazal <aklimakajal11@gmail.com>
2025-01-21 19:26:30 +0600

    new-add

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-gray-700 text-center">Hello</h1>
+      <p className="text-center text-base text-gray-600">
+        this is my first next project
+      </p>
+    </>
+  );
+}
